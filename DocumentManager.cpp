f4ea3f76d2490c5e63d82edcfb4b5cf032b17bb5
Vic <thanhvic02@gmail.com>
2023-05-29 20:03:24 +0930

    finish addPatron and search function

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,2 +1,12 @@
+#include "DocumentManager.h"
 
+void DocumentManager::addPatron(int patronID){
+    patrons.insert(patronID);
+}
 
+int DocumentManager::search(string name){
+    if (name_to_docid.count(name) > 0){
+        return name_to_docid[name];
+    }
+    return 0;
+}
--- DocumentManager.h
+++ DocumentManager.h
@@ -1,2 +1,26 @@
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
+
+class DocumentManager{
+    private:
+        struct Document{
+            Document(std::string name, int id, int license_limit);
+
+            std::string name;
+            int id;
+            int license_limit;
+            int current_copies_borrowed;
+        }
+
+        std::unordered_map<int, Document> documents;
+        std::unordered_set<int> patrons;
+        std::unordered_map<int> name_to_docid;
+
+    public:
+        void addDocument(string name, int id, int license_limit);
+        void addPatron(int patronID);
+        int search(string name);
+        bool borrowDocument(int docid, int patronID);
+        void returnDocument(int docid, int patronID);
+};
